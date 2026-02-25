e9abf41dad9a146a668e48302c45c9037ff141e1
rainnercardoso459-cmd <rainnercardoso459@gmail.com>
2026-02-24 21:09:36 -0300

    Create produtos.js

---
+++ produtos.js
@@ -0,0 +1,43 @@
+const produtos = [
+
+{
+id:1,
+nome:"Nike Air Jordan 1",
+preco:499.90,
+estoque:5,
+imagem:"images/jordan.png"
+},
+
+{
+id:2,
+nome:"Nike Air Max",
+preco:459.90,
+estoque:4,
+imagem:"images/airmax.png"
+},
+
+{
+id:3,
+nome:"Adidas Superstar",
+preco:399.90,
+estoque:7,
+imagem:"images/superstar.png"
+},
+
+{
+id:4,
+nome:"Puma RS-X",
+preco:379.90,
+estoque:3,
+imagem:"images/rsx.png"
+},
+
+{
+id:5,
+nome:"Lacoste Powercourt",
+preco:459.90,
+estoque:2,
+imagem:"images/lacoste.png"
+}
+
+]
